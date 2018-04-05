ed8eee8a520db743592ca15c7ab1399f4bbf9849
Ricardo Garrido <ricgarridoc@hotmail.com>
2018-04-05 02:17:49 -0500

    I made some exercises with loops with while,for and do while

---
+++ if-else.php
@@ -0,0 +1,163 @@
+<?php
+/*Verifica si el número es positivo, negativo o cero
+$num = -1;
+if ($num > 0) {
+	echo "Eres un número positivo";
+}elseif ($num == 0) {
+	echo "El numero es 0";
+}else{
+	echo "el numero es negativo";
+}
+*/
+$mes = 42;
+switch ($mes) {
+	case 1:
+		echo "Enero";
+		break;
+
+	case 2:
+		echo "Febrero";
+		break;
+	
+	case 3:
+		echo "Marzo";
+		break;
+
+	case 4:
+		echo "Abril, el mejor mes, ahuevo!";
+		break;
+
+	case 5:
+		echo "Mayo";
+		break;
+
+	case 6:
+		echo "Junio";
+		break;
+
+	case 7:
+		echo "Julio";
+		break;
+	
+	case 8:
+		echo "Agosto";
+		break;
+
+	case 9:
+		echo "Septiembre";
+		break;
+
+	case 10:
+		echo "Octubre";
+		break;
+
+	case 11:
+		echo "Noviembre";
+		break;
+
+	case 12:
+		echo "Diciembre";
+		break;	
+	
+	default:
+		echo "Ese pinshi mes no existe morro, alv!";
+		break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*Edades
+	0-2 bebe
+	3-12 niño
+	13-17 adolescente
+	18 en adelante Adulto
+
+	$edad = 11;
+	if ($edad <= 2) {
+		echo "Es un bebe";
+	}elseif ($edad <= 12) {
+		echo "Es niño";
+	}elseif ($edad <= 17) {
+		echo "Es adolescente";
+	}else{
+		echo "Es adulto";
+	}
+*/
+
+/*Usando switch/case/default y break
+	$numero = 8;
+	switch ($numero) {
+		case 1:
+			echo "Es Lunes, principio de semana";
+			break;
+
+		case 2:
+			echo "Martes, 2x1 en las pizzerias";
+		break;
+		
+		case 3:
+			echo "Miercoles, 2x1 en todos los cines";
+			break;
+
+		case 4:
+			echo "Juebebes";
+			break;
+
+		case 5:
+			echo "Viernes de putas";
+			break;
+
+		case 6:
+			echo "Sabadrink";
+			break;
+
+		default:
+			echo "Es Domingo, día de hacer el aseo en la pishi casa";
+			break;
+	}
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<form action="">
+<input type="url" placeholder="http://ricgc.com">
+</form>
+</body>
+</html>
---
+++ loops.php
@@ -0,0 +1,70 @@
+<?php
+/*
+$num = 0;
+while ($num <= 10) {
+	echo $num ." - ";
+	$num++;
+}
+*/
+
+
+/* Imprime los números pares
+$n = 0;
+while ($n < 50) {
+	echo $n = $n + 2 . "<br>";
+}
+*/
+
+/* Imprime numeros impares
+$n = 1;
+while ($n < 49) {
+ 	echo $n = $n + 2 . "<br>";
+ } 
+*/
+
+/* 2 elevado en diferentes potencias
+$p = 2;
+for ($i=1; $i <= 10; $i++) { 
+	echo $p . " elevado a la " . $i ." es igual a = ". $p**$i . "<br>";
+}
+*/
+
+
+/*Ciclo denteo de un ciclo
+$num = 1;
+while ( $num <= 10) {
+	echo "La tabla del " . $num . "<br>";
+	$num2 = 1;
+	while ( $num2 <= 10) {
+		echo $num . " x ". $num2 . " = " . $num * $num2 . "<br>";
+		$num2++;
+	}
+
+$num++;
+}
+
+*/
+
+
+
+
+
+
+
+/*
+for ($i=0; $i <= 10; $i++) { 
+	echo $i ." - ";
+}
+*/
+
+
+
+/*Do es un ciclo que aunque este mal declarado el ciclo, a fuerza se va a ejecutar una vez.
+
+$num = 101;
+do{
+	echo $num ." - ";
+	$num++;
+}while ($num <= 10)
+*/
+?>
